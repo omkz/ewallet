5d57703d01fb0804791ec59f219963dbc2903b45
Kurnia Muhamad <omenkzz@gmail.com>
2019-09-10 23:09:54 +0700

    Fix deposit typo

--- app/models/deposit.rb
+++ app/models/deposit.rb
@@ -2,11 +2,11 @@ class Deposit < Transaction
   def create
     ActiveRecord::Base.transaction do
       self.save!
-      self.update_stock
+      self.update_balance
     end
   end
 
-  def update_stock
+  def update_balance
     user.update! balance: user.balance + amount.to_i
   end
 end
