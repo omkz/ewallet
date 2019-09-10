e95b812d0346416025ba53b83c74577baa6662dd
Kurnia Muhamad <omenkzz@gmail.com>
2019-09-10 22:08:06 +0700

    Add user and authentication

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,9 @@ module Ewallet
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.generators do |g|
+      g.helper false
+      g.assets false
+    end
   end
 end
